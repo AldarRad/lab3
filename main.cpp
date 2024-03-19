b2d5f070f2fbc85f7fce5562696a9ce56dab1dcb
AldarRad <rald2005@mail.ru>
2024-03-19 15:11:49 +0700

    3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <vector>
+int main()
+{
+	const int n = 15;
+	std::vector <int> a(n);
+	for(int i=0;i<n;i++)
+	{
+		std::cin >> a[i];
+	}
+	int number;
+	std::cin >> number;
+	for (int i = 0;i < a.size();i++)
+	{
+		if (a[i] == number)
+		{
+			a.erase(a.begin() + i);			
+		}
+	}
+	for(int i=0;i<a.size();i++)
+	{
+		std::cout << a[i] << " ";
+	}
 
-int main(int, char**){
-    std::cout << "Hello, from lab3!\n";
 }
